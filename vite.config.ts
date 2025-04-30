9cc9231477a815932d1807c06114c75b76f90067
hava <hava.nagdi@gmail.com>
2025-04-30 16:36:16 -0700

    Update vite config and remove public assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '',
+  base: process.env.NODE_ENV === 'production' ? '/browser-game/' : '/',
   server: {
     host: 'localhost',
     port: 5173,
